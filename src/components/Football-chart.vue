90d5c9cff8f666ae5016a64990300fd591ccac97
Philip Persson <51906227+PhPersson@users.noreply.github.com>
2023-04-16 17:13:46 +0200

    Kan plocka ut bortalagen från dagens matcher

--- src/components/Football-chart.vue
+++ src/components/Football-chart.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="hello">
    <h1>Hej!</h1>
+   <ol>
+   <li v-for="team in awayTeams" v-bind:key="team.id">{{ team.name }}</li>
+  </ol>
   </div>
 </template>
 
@@ -13,6 +16,11 @@ export default {
   name: 'FootballChart',
 
   data() {
+    return {
+      PLurl: 'https://api.football-data.org/v4/competitions/PL/matches?season=2022&dateFrom=2023-04-16&dateTo=2023-04-16',
+      awayTeams: [],
+      homeTeams: [],
+    }
   },
 
   mounted() {
@@ -22,7 +30,7 @@ export default {
 
   methods: {
     fetchApiData(){
-      const url = 'https://api.football-data.org/v4/competitions/PL/matches?season=2022&dateFrom=2023-04-16&dateTo=2023-04-16';
+      const url = this.PLurl;
       const options = {
         headers: {
           'X-Auth-Token': `${process.env.VUE_APP_API_KEY}`
@@ -30,14 +38,30 @@ export default {
       };
       axios.get(url, options)
         .then(response => {
-          console.log(response.data.matches[1].awayTeam);
+          this.handleAwayTeams(response.data.matches);
+          this.handleHomeTeams(response.data.matches);
         })
         .catch(error => {
           console.error(error.message);
         });
+  },
 
+    handleAwayTeams(matches) { //Tar in alla matcher som indata-parameter
+      matches.forEach(match => {
+        this.awayTeams.push(match.awayTeam)
+      });
 
-  }
+      this.awayTeams.forEach(element => {
+        console.log(element);
+      });
+    },
+
+    handleHomeTeams(matches) { //Tar in alla matcher som indata-parameter
+      matches.forEach(match => {
+        this.homeTeams.push(match.homeTeam);
+      });
+
+    },
 }
 
 
