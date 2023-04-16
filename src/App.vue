06cd060f21492ad0b677e2029c62017997ff97f5
olsson7 <72479962+olsson7@users.noreply.github.com>
2023-04-16 15:05:15 +0200

    Init projekt

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.jpg">
+  <FootballChartVue/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FootballChartVue from './components/Football-chart.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    FootballChartVue
   }
 }
 </script>
