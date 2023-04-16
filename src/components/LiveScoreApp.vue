868bc518448781540ef8a0be856a7f2504c7ba4e
Philip Persson <51906227+PhPersson@users.noreply.github.com>
2023-04-16 21:38:46 +0200

    Implemented saving to localStorage

--- src/components/LiveScoreApp.vue
+++ src/components/LiveScoreApp.vue
@@ -7,28 +7,23 @@
     
       
       <li class="match" v-for="match in matchesToday" :key="match.id">
-        
-
-
+      
         <p class="time" v-if="match.status === 'FINISHED'"> {{ getTime(match) + ' CEST' }} -  FULL TIME  </p>
         <p class="time" v-if="match.status === 'IN_PLAY' && match.score.halfTime.home === null"> {{ getTime(match) + ' CEST' }} -  LIVE first half  </p>
         <p class="time" v-if="match.status === 'IN_PLAY' && match.score.halfTime.home !== null"> {{ getTime(match) + ' CEST' }} -  LIVE second half  </p>
         <p class="time" v-else-if="match.status === 'PAUSED'"> {{ getTime(match) + ' CEST' }} -  HT  </p>
         <p class="time" v-else-if="match.status === 'TIMED'"> {{ getTime(match) + ' CEST' }}  </p>
 
-
-
-
-       <img v-bind:src="match.homeTeam.crest" class="crest">
-
-
-        {{ match.homeTeam.name }} 
-
-        <div class="score"> {{ match.score.fullTime.home }} - {{ match.score.fullTime.away }} </div>
-
-        {{ match.awayTeam.name }}
-
-        <img v-bind:src="match.awayTeam.crest" class="crest">
+        <img v-bind:src="match.homeTeam.crest" class="crest" />
+        <div class="homeTeam">
+          {{ match.homeTeam.name }} <Icon icon="bx:football" @click="saveTeam(match.homeTeam.id, match.homeTeam.name)"/>
+        </div>
+        <div class="score">
+          {{ match.score.fullTime.home }} - {{ match.score.fullTime.away }}
+        </div>
+        <div class="awayTeam">
+          {{ match.awayTeam.name }} <Icon icon="bx:football" @click="saveTeam(match.awayTeam.id, match.awayTeam.name)"/>
+        </div>
 
 
       </li>
@@ -39,9 +34,12 @@
 <script>
 
 import axios from 'axios';
-
+import { Icon } from '@iconify/vue';
 export default {
   name: 'LiveScoreApp',
+  components: {
+      Icon,
+    },
   data() {
     return {
       // PLurl: 'https://api.football-data.org/v4/competitions/PL/matches',
@@ -100,6 +98,11 @@ export default {
         console.error(error.message)
       }
   },
+  saveTeam(teamID, teamName) {
+      const teamList = JSON.parse(localStorage.getItem('teamList')) || [];
+      teamList.push({teamID, teamName});
+      localStorage.setItem('teamList', JSON.stringify(teamList));
+    }
 }
 
 
