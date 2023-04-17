49a572e5e1685dd57b0fd2c6b970813f9a047c70
Philip Persson <51906227+PhPersson@users.noreply.github.com>
2023-04-17 09:40:38 +0200

    Added so app fetch fav from localstorage and also an error message if fav is empty

--- src/components/LiveScoreApp.vue
+++ src/components/LiveScoreApp.vue
@@ -1,40 +1,38 @@
 <template>
   <div class="live">
-    <h1>Your favorites:</h1>
-    <ul>
+    <div class="favTeams">
+      <h1>Your favorites:</h1>
+        <ul v-if="favTeams.length > 0">
+
+          <li class="match" v-for="match in favTeams" :key="match.id">
+            <p class="date">Date: {{ match.utcDate.substring(0, 10) }} </p>
+            <p class="time" v-if="match.status === 'FINISHED'"> {{ getTime(match) + ' CEST' }} - FULL TIME </p>
+            <div class="time" v-else-if="match.status === 'IN_PLAY' && match.score.halfTime.home === null"> {{ getTime(match)
+              + ' CEST' }} - <p class="timeLive">LIVE</p> first half </div>
+            <div class="time" v-else-if="match.status === 'IN_PLAY' && match.score.halfTime.home !== null"> {{ getTime(match)
+              + ' CEST' }} - <p class="timeLive">LIVE</p> second half </div>
+            <p class="time" v-else-if="match.status === 'PAUSED'"> {{ getTime(match) + ' CEST' }} - HT </p>
+            <p class="time" v-else-if="match.status === 'TIMED'"> {{ getTime(match) + ' CEST' }} </p>
 
 
+            <div class="homeTeam">
+              <Icon class="faicon" icon="ic:outline-star-border" @click="saveTeamAsFav(match.homeTeam.id, match.homeTeam.name)" />
+              <img v-bind:src="match.homeTeam.crest" class="crest" />
+              {{ match.homeTeam.name }}
+            </div>
+            <div class="score">
+              {{ match.score.fullTime.home }} - {{ match.score.fullTime.away }}
+            </div>
+            <div class="awayTeam">
+              {{ match.awayTeam.name }}
+              <img v-bind:src="match.awayTeam.crest" class="crest" />
+              <Icon class="faicon" icon="ic:outline-star-border" @click="saveTeamAsFav(match.awayTeam.id, match.awayTeam.name)" />
+            </div>
 
-<li class="match" v-for="match in matchesFav" :key="match.id">
-
-  <p class="date">Date: {{ match.utcDate.substring(0, 10) }} </p>
-  <p class="time" v-if="match.status === 'FINISHED'"> {{ getTime(match) + ' CEST' }} - FULL TIME </p>
-  <div class="time" v-else-if="match.status === 'IN_PLAY' && match.score.halfTime.home === null"> {{ getTime(match)
-    + ' CEST' }} - <p class="timeLive">LIVE</p> first half </div>
-  <div class="time" v-else-if="match.status === 'IN_PLAY' && match.score.halfTime.home !== null"> {{ getTime(match)
-    + ' CEST' }} - <p class="timeLive">LIVE</p> second half </div>
-  <p class="time" v-else-if="match.status === 'PAUSED'"> {{ getTime(match) + ' CEST' }} - HT </p>
-  <p class="time" v-else-if="match.status === 'TIMED'"> {{ getTime(match) + ' CEST' }} </p>
-
-
-  <div class="homeTeam">
-    <Icon class="faicon" icon="ic:outline-star-border" @click="saveTeam(match.homeTeam.id, match.homeTeam.name)" />
-    <img v-bind:src="match.homeTeam.crest" class="crest" />
-    {{ match.homeTeam.name }}
-  </div>
-  <div class="score">
-    {{ match.score.fullTime.home }} - {{ match.score.fullTime.away }}
-  </div>
-  <div class="awayTeam">
-    {{ match.awayTeam.name }}
-    <img v-bind:src="match.awayTeam.crest" class="crest" />
-    <Icon class="faicon" icon="ic:outline-star-border" @click="saveTeam(match.awayTeam.id, match.awayTeam.name)" />
-  </div>
-
-
-</li>
-</ul>
-
+          </li>
+        </ul>
+        <p id="nofav" v-else>No favorite teams added yet.</p>
+    </div>
 
 
     <div class="topMenu">
@@ -49,10 +47,7 @@
 
 
 
-
     <ul>
-
-
 
       <li class="match" v-for="match in matchesToday" :key="match.id">
 
@@ -66,7 +61,7 @@
 
 
         <div class="homeTeam">
-          <Icon class="faicon" icon="ic:outline-star-border" @click="saveTeam(match.homeTeam.id, match.homeTeam.name)" />
+          <Icon class="faicon" icon="ic:outline-star-border" @click="saveTeamAsFav(match.homeTeam.id, match.homeTeam.name)" />
           <img v-bind:src="match.homeTeam.crest" class="crest" />
           {{ match.homeTeam.name }}
         </div>
@@ -76,9 +71,8 @@
         <div class="awayTeam">
           {{ match.awayTeam.name }}
           <img v-bind:src="match.awayTeam.crest" class="crest" />
-          <Icon class="faicon" icon="ic:outline-star-border" @click="saveTeam(match.awayTeam.id, match.awayTeam.name)" />
+          <Icon class="faicon" icon="ic:outline-star-border" @click="saveTeamAsFav(match.awayTeam.id, match.awayTeam.name)" />
         </div>
-
 
       </li>
     </ul>
@@ -86,7 +80,6 @@
 </template>
 
 <script>
-
 import axios from 'axios';
 import { Icon } from '@iconify/vue';
 
@@ -110,41 +103,29 @@ export default {
   data() {
     return {
 
-
       state: State.Today,
 
 
 
-      // PLurl: 'https://api.football-data.org/v4/competitions/PL/matches',
-      // SerieAUrl: `https://api.football-data.org/v4/competitions/SA/matches`,
-      // BundesUrl: `https://api.football-data.org/v4/competitions/BL1/matches`,
       apiUrl: `https://api.football-data.org/v4/matches?competitions=2002,2019,2014,2015,2021`,
       apiUrlYesterday: `https://api.football-data.org/v4/matches?competitions=2002,2019,2014,2015,2021&date=YESTERDAY`,
       apiUrlTomorrow: `https://api.football-data.org/v4/matches?competitions=2002,2019,2014,2015,2021&date=TOMORROW`,
       apiUrlFav1: `https://api.football-data.org/v4/teams/`,
       apiUrlFav2: `/matches?dateFrom=2023-04-16&dateTo=2023-04-30`,
 
-
-      //Gårdagens resultat https://api.football-data.org/v4/matches?competitions=2002,2019,2014,2015,2021&date=YESTERDAY
-      //morgondagens matcher https://api.football-data.org/v4/matches?competitions=2002,2019,2014,2015,2021&date=TOMORRROW
-
-
-
       //Specifikt team matcher https://api.football-data.org/v4/teams/99/matches?dateFrom=2023-04-16&dateTo=2023-04-30
 
       // Kommande matcher https://api.football-data.org/v4/teams/5890/matches?dateFrom=2023-04-16&dateTo=2023-04-30
 
-
-
       matchesToday: [],
-      matchesFav: [],
+      favTeams: [],
       todaysDate: ''
     }
   },
 
   async mounted() {
-    //this.getTodaysDate();
     teamIDtemp = await this.getTeam();
+    this.fetchFavTeams();
     await this.fetchApiData(this.apiUrl);
     await this.fetchApiDataFav(this.apiUrlFav1, this.apiUrlFav2, teamIDtemp[0].teamID);
   },
@@ -190,7 +171,6 @@ export default {
 
 
     async fetchApiDataFav(url, url2, fav) {
-
       const options = {
         headers: {
           'X-Auth-Token': `${process.env.VUE_APP_API_KEY}`
@@ -207,7 +187,7 @@ export default {
         const response = await axios.get(url, options);
         //this.matchesToday = response.data.matches;
         console.log(response.data.matches);
-        this.matchesFav = response.data.matches;
+        this.favTeams = response.data.matches;
 
       } catch (error) {
         console.error(error.message);
@@ -233,9 +213,7 @@ export default {
         url = this.apiUrl;
         this.state = State.Today;
 
-
       }
-
       const options = {
         headers: {
           'X-Auth-Token': `${process.env.VUE_APP_API_KEY}`
@@ -255,20 +233,28 @@ export default {
       }
     },
 
+    saveTeamAsFav(teamID, teamName) {
+      this.favTeams = JSON.parse(localStorage.getItem('favTeams')) || [];
+      this.favTeams.push({ teamID, teamName });
+      localStorage.setItem('favTeams', JSON.stringify(this.favTeams));
+    },
 
-
-
-
-
-    saveTeam(teamID, teamName) {
-      const teamList = JSON.parse(localStorage.getItem('teamList')) || [];
-      teamList.push({ teamID, teamName });
-      localStorage.setItem('teamList', JSON.stringify(teamList));
+    fetchFavTeams() {
+      try {
+      const storedFavTeams = localStorage.getItem('favTeams');
+      if (storedFavTeams) {
+        this.favTeams = JSON.parse(storedFavTeams);
+        } else {
+        this.favTeams = [];
+        }
+      } catch (error) {
+      console.error('Error parsing favTeams from localStorage:', error);
+      this.favTeams = [];
+      }
+    },
+  
+  
     }
-
-
-  }
-
 
 }
 </script>
