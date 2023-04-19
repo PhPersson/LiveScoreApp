023bcb916cd2ed6211fc04a36a1965d18804304d
olsson7 <72479962+olsson7@users.noreply.github.com>
2023-04-20 01:57:23 +0200

    Live + Table page. Send url id from navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,59 +1,88 @@
 <template>
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="/">    <img src="https://www.svgrepo.com/show/17072/football.svg" width="60" alt="Bootstrappin'">
-LiveScoreApp</a>
-
-
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/">
-            Home
-            </a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Live games
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+          <a class="navbar-brand" href="/"> <img src="https://www.svgrepo.com/show/17072/football.svg" width="60"
+                  alt="Bootstrappin'">
+              LiveScoreApp
           </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="/about">Premier League</a></li>
-            <li><a class="dropdown-item" href="/about">La Liga</a></li>
-            <li><a class="dropdown-item" href="/about">Serie A</a></li>
-            <li><a class="dropdown-item" href="/about">BundesLiga</a></li>
-            <li><a class="dropdown-item" href="/about">League 1</a></li>
-
-          </ul>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            League Tables
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="/about">Premier League</a></li>
-            <li><a class="dropdown-item" href="/about">La Liga</a></li>
-            <li><a class="dropdown-item" href="/about">Serie A</a></li>
-            <li><a class="dropdown-item" href="/about">BundesLiga</a></li>
-            <li><a class="dropdown-item" href="/about">League 1</a></li>
-          </ul>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
 
 
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+              aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavDropdown">
+              <ul class="navbar-nav">
+                  <li class="nav-item">
+                      <a class="nav-link active" aria-current="page" href="/">
+                          Home
+                      </a>
+                  </li>
+                  <li class="nav-item dropdown">
+                      <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                          aria-expanded="false">
+                          Live games
+                      </a>
+                      <ul class="dropdown-menu">
+                          <li><a class="dropdown-item" id="PL" v-bind:href="'/live/'+ this.link" @click="select($event)">Premier League</a></li>
+                          <li><a class="dropdown-item" id="PD" v-bind:href="'/live/'+ this.link" @click="select($event)">La Liga</a></li>
+                          <li><a class="dropdown-item" id="SA" v-bind:href="'/live/'+ this.link" @click="select($event)">Serie A</a></li>
+                          <li><a class="dropdown-item" id="Bl1" v-bind:href="'/live/'+ this.link" @click="select($event)">BundesLiga</a></li>
+                          <li><a class="dropdown-item" id="FL1" v-bind:href="'/live/'+ this.link" @click="select($event)">League 1</a></li>
+
+                      </ul>
+                  </li>
+                  <li class="nav-item dropdown">
+                      <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                          aria-expanded="false">
+                          League Tables
+                      </a>
+                      <ul class="dropdown-menu">
+                          <li><a class="dropdown-item" id="PL" v-bind:href="'/table/'+ this.link" @click="select($event)">Premier League</a></li>
+                          <li><a class="dropdown-item" id="PD" v-bind:href="'/table/'+ this.link" @click="select($event)">La Liga</a></li>
+                          <li><a class="dropdown-item" id="SA" v-bind:href="'/table/'+ this.link" @click="select($event)">Serie A</a></li>
+                          <li><a class="dropdown-item" id="Bl1" v-bind:href="'/table/'+ this.link" @click="select($event)">BundesLiga</a></li>
+                          <li><a class="dropdown-item" id="FL1" v-bind:href="'/table/'+ this.link" @click="select($event)">League 1</a></li>
+                      </ul>
+                  </li>
+              </ul>
+          </div>
+      </div>
+  </nav>
 </template>
 
 
 <script>
-export default{
-    name: "NavBar"
-    
+
+export default {
+  name: "NavBar",
+
+  data() {
+      return {    
+        link: ""    
+      }
+
+  },
+ 
+
+ 
+
+
+  methods: {
+
+    select: function(event){
+      this.link = event.target.id;
+      console.log(this.link);
+    }
+
+
+  }
+
 }
+
+
+
+
 
 
 </script>
---
+++ src/views/Live.vue
@@ -0,0 +1,29 @@
+<template>
+<h1> Live Games {{ league }}</h1>
+   
+  </template>
+  
+  
+  <script>
+  import LiveScoreApp from '@/components/LiveScoreApp.vue'
+  
+  export default {
+    name: 'App',
+    components: {
+      LiveScoreApp
+    },
+
+    data() {
+      return {    
+        league: this.$route.params.league    
+      }
+
+    },
+    methods: {
+      reloadPage() {
+        location.reload();
+      }
+    }
+  }
+  
+  </script>
