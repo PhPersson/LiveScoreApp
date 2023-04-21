65765a5afd2ab879acbf3e851e353edb3529602d
Philip Persson <51906227+PhPersson@users.noreply.github.com>
2023-04-21 15:46:57 +0200

    Cleaned up the code in LiveScoreApp.vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import { createWebHistory, createRouter } from 'vue-router';
 import Home from '@/views/Home.vue';
-import About from '@/views/About.vue';
 import Table from '@/views/Table.vue';
 import Live from '@/views/Live.vue';
 import Favorites from '@/views/Favorites.vue';
@@ -8,12 +7,9 @@ import Favorites from '@/views/Favorites.vue';
 
 const routes = [
   { path: '/', name: 'Home', component: Home},
-  { path: '/about', name: 'About', component: About},
   { path: '/table/:league', name: 'Table', component: Table},
   { path: '/live/:league', name: 'Live', component: Live},
   { path: '/Favorites/', name: 'Favorites', component: Favorites}
-
-
 ];
 
 const router = createRouter({
--- src/views/Favorites.vue
+++ src/views/Favorites.vue
@@ -3,3 +3,26 @@
 <template>
     <h1>Hej</h1>
 </template>
+
+<!-- async fetchApiDataFav(url, url2, fav) { //Hantera om favoriter är null
+
+    const options = {
+      headers: {
+        'X-Auth-Token': `${process.env.VUE_APP_API_KEY}`
+      },
+      params: {
+
+      }
+    };
+    url = url + fav + url2;
+
+    try {
+      const response = await axios.get(url, options);
+      //this.matchesToday = response.data.matches;
+      console.log(response.data.matches);
+      this.matchesFav = response.data.matches[0];
+
+    } catch (error) {
+      console.error(error.message);
+    }
+  }, -->
