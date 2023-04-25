7a3c5e47a06a88b2657be218e726f50753520fa1
Philip Persson <51906227+PhPersson@users.noreply.github.com>
2023-04-25 17:06:11 +0200

    Added a footer

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,24 @@
 <template>
   <NavBar/>
   <router-view />
+  <footer class="bg-light text-center text-lg-start">
+  <!-- Copyright -->
+  <div class="text-center p-1" style="background-color: rgba(0, 0, 0, 0.2);">
+    <p class="text-dark" > LiveScoreApp <a class="btn" href="https://github.com/PhPersson/LiveScoreApp" role="button"> 
+          <Icon icon="icon-park:github" /></a></p>
+  </div>
+  <!-- Copyright -->
+</footer>
 </template>
 
 <script>
 import NavBar from './components/NavBar.vue';
-
+import { Icon } from '@iconify/vue';
 export default {
   name: 'App',
   components: {
-    NavBar
+    NavBar,
+    Icon
   },
   methods: {
     reloadPage() {
