6d2de4f889c7bfbde2d7bd0a29b3be02706348a0
Philip Persson <51906227+PhPersson@users.noreply.github.com>
2023-04-30 22:54:04 +0200

    Refctored the code

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -9,49 +9,50 @@
         </li>
       </ul>
     </div>
-  </template>
+</template>
   
 <script>
 
 import axios from 'axios';
-  export default {
+
+export default {
     data() {
-      return {
-        allTeams: [],
-        searchTerm: '',
-      };
+        return {
+            allTeams: [],
+            searchTerm: '',
+        };
     },
     async mounted() {
         await this.fetchApiData();
     },
     computed: {
-      filteredTeams() {
+        filteredTeams() {
         return this.allTeams.filter(team =>
-          team.name.toLowerCase().includes(this.searchTerm.toLowerCase())
+            team.name.toLowerCase().includes(this.searchTerm.toLowerCase())
         );
-      },
+        },
     },
 
     methods: {
-    async fetchApiData() {
-      const options = {
-          headers: {
-              'X-Auth-Token': `${process.env.VUE_APP_API_KEY}`
-          },
-          params: {
-              limit: 500,
-          }
-      };
-      var url = `https://api.football-data.org/v4/teams`;
-      try {
-          const response = await axios.get(url, options);
-            console.log(response.data.teams);
-            this.allTeams = response.data.teams
-      } catch (error) {
-          console.error(error.message);
-      }
-      },     
+        async fetchApiData() {
+            const options = {
+                headers: {
+                    'X-Auth-Token': `${process.env.VUE_APP_API_KEY}`
+                },
+                params: {
+                    limit: 500,
+                }
+            };
+            var url = `https://api.football-data.org/v4/teams`;
+            try {
+                const response = await axios.get(url, options);
+                console.log(response.data.teams);
+                this.allTeams = response.data.teams
+            } catch (error) {
+                console.error(error.message);
+            }
+        },     
     }
-  };
+};
 </script>
   
