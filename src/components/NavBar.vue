7010a1ff72a45ffee9427211e43ed2a92853031f
olsson7 <72479962+olsson7@users.noreply.github.com>
2023-05-23 22:38:22 +0200

    Search autocomplete navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,79 +1,152 @@
 <template>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="/"> <img src="https://www.svgrepo.com/show/17072/football.svg"
-                    width="60">LiveScoreApp </a>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="/"> <img src="https://www.svgrepo.com/show/17072/football.svg" width="60">LiveScoreApp
+      </a>
 
 
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
-                aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="/"> Home </a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                            aria-expanded="false"> Today's matches </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" id="PL" v-bind:href="'/live/' + this.link"
-                                    @click="select($event)">Premier League</a></li>
-                            <li><a class="dropdown-item" id="PD" v-bind:href="'/live/' + this.link"
-                                    @click="select($event)">La Liga</a></li>
-                            <li><a class="dropdown-item" id="SA" v-bind:href="'/live/' + this.link"
-                                    @click="select($event)">Serie A</a></li>
-                            <li><a class="dropdown-item" id="BL1" v-bind:href="'/live/' + this.link"
-                                    @click="select($event)">BundesLiga</a></li>
-                            <li><a class="dropdown-item" id="FL1" v-bind:href="'/live/' + this.link"
-                                    @click="select($event)">League 1</a></li>
-                            <li><a class="dropdown-item" id="CL" v-bind:href="'/live/' + this.link"
-                                    @click="select($event)">Champions League</a></li>
-                        </ul>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                            aria-expanded="false"> League tables </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" id="PL" v-bind:href="'/table/' + this.link"
-                                    @click="select($event)">Premier League</a></li>
-                            <li><a class="dropdown-item" id="PD" v-bind:href="'/table/' + this.link"
-                                    @click="select($event)">La Liga</a></li>
-                            <li><a class="dropdown-item" id="SA" v-bind:href="'/table/' + this.link"
-                                    @click="select($event)">Serie A</a></li>
-                            <li><a class="dropdown-item" id="BL1" v-bind:href="'/table/' + this.link"
-                                    @click="select($event)">BundesLiga</a></li>
-                            <li><a class="dropdown-item" id="FL1" v-bind:href="'/table/' + this.link"
-                                    @click="select($event)">League 1</a></li>
-                        </ul>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/favorites" role="button" aria-expanded="false"> Favorites </a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+        aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="/"> Home </a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Today's matches </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" id="PL" v-bind:href="'/live/' + this.link" @click="select($event)">Premier
+                  League</a></li>
+              <li><a class="dropdown-item" id="PD" v-bind:href="'/live/' + this.link" @click="select($event)">La Liga</a>
+              </li>
+              <li><a class="dropdown-item" id="SA" v-bind:href="'/live/' + this.link" @click="select($event)">Serie A</a>
+              </li>
+              <li><a class="dropdown-item" id="BL1" v-bind:href="'/live/' + this.link"
+                  @click="select($event)">BundesLiga</a></li>
+              <li><a class="dropdown-item" id="FL1" v-bind:href="'/live/' + this.link" @click="select($event)">League
+                  1</a></li>
+              <li><a class="dropdown-item" id="CL" v-bind:href="'/live/' + this.link" @click="select($event)">Champions
+                  League</a></li>
+            </ul>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              League tables </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" id="PL" v-bind:href="'/table/' + this.link" @click="select($event)">Premier
+                  League</a></li>
+              <li><a class="dropdown-item" id="PD" v-bind:href="'/table/' + this.link" @click="select($event)">La Liga</a>
+              </li>
+              <li><a class="dropdown-item" id="SA" v-bind:href="'/table/' + this.link" @click="select($event)">Serie A</a>
+              </li>
+              <li><a class="dropdown-item" id="BL1" v-bind:href="'/table/' + this.link"
+                  @click="select($event)">BundesLiga</a></li>
+              <li><a class="dropdown-item" id="FL1" v-bind:href="'/table/' + this.link" @click="select($event)">League
+                  1</a></li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/favorites" role="button" aria-expanded="false"> Favorites </a>
+          </li>
+
+
+          <li class="nav-item" id="Searchbar">
+
+            <v-autocomplete :items="teamList" :custom-filter="customFilter" color="white" item-title="name"
+              item-value="id" label="Search teams" return-object v-model="selectedClub"></v-autocomplete>
+            <v-btn id="Search" text color="teal" @click="submit">Search</v-btn>
+
+          </li>
+        </ul>
+      </div>
+
+    </div>
+
+  </nav>
 </template>
 
 
 <script>
 
-export default {
-    name: "NavBar",
+import axios from 'axios';
 
-    data() {
-        return {
-            link: ""
-        }
+
+export default {
+  name: "NavBar",
+
+  data() {
+    return {
+      link: "",
+      url: `https://api.football-data.org/v4/teams/`,
+      search: null,
+      select: null,
+      teamList: [],
+
+      selectedClub: { id: null, name: null }
+    }
+  },
+
+
+
+  watch: {
+    search(val) {
+      val && val !== this.select && this.querySelections(val)
+    },
+  },
+
+  mounted() {
+    this.selectedClub = { id: null, name: null };
+    this.fetchApiData(this.url);
+  },
+
+  methods: {
+    select: function (event) {
+      this.link = event.target.id;
     },
 
-    methods: {
-        select: function (event) {
-            this.link = event.target.id;
+
+    submit() {
+      console.log(this.selectedClub.id)
+    },
+
+    async fetchApiData(url) {
+      const options = {
+        headers: {
+          'X-Auth-Token': process.env.VUE_APP_API_KEY,
         },
-    }
+        params: {
+          limit: 500,
+        }
+      };
+
+      try {
+        const response = await axios.get(url, options);
+
+        const teams = response.data.teams;
+
+
+        teams.forEach((team) => {
+          this.teamList.push({
+            id: team.id,
+            name: team.name,
+          });
+        });
+        console.log(response.data.teams);
+      } catch (error) {
+        console.error('API request failed:', error.message);
+      }
+    },
+
+
+
+  },
+
+
+
+
 
 }
 
