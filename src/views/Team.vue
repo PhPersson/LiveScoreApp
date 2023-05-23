3d848641ce47fdf4d7cfd51b171a68d4c1af1544
olsson7 <72479962+olsson7@users.noreply.github.com>
2023-05-23 23:47:56 +0200

    Added Team page

---
+++ src/views/Team.vue
@@ -0,0 +1,31 @@
+<template>
+     <h1> {{ team }}</h1>
+</template>
+  
+  
+<script>
+import LiveScoreApp from '@/components/LiveScoreApp.vue'
+import axios from 'axios';
+
+export default {
+    name: 'App',
+    components: {
+        
+    },
+
+    data() {
+        return {
+            team: this.$route.params.id,
+        }
+    },
+
+    async mounted() {
+        
+    },
+
+    methods: {
+        
+    }
+}
+</script>
+<style src="..\css\LiveScoreApp.css"></style>
