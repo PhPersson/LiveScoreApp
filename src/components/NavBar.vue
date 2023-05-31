042658d49031943cf970adcd88584994432a98ac
Philip Persson <51906227+PhPersson@users.noreply.github.com>
2023-05-31 14:49:33 +0200

    Fixed an buffer problem in the navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -5,11 +5,16 @@
         <img src="https://www.svgrepo.com/show/17072/football.svg" width="60" />
         LiveScoreApp
       </a>
-      <div class="navbar-collapse justify-content-between" id="navbarNavDropdown">
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+      <div class="collapse navbar-collapse" id="navbarNavDropdown">
         <ul class="navbar-nav">
           <li class="nav-item">
             <a class="nav-link active" aria-current="page" href="/">Home</a>
           </li>
+
+           
           <li class="nav-item dropdown">
             <a
               class="nav-link dropdown-toggle"
@@ -201,7 +206,8 @@ export default {
             'X-Auth-Token': process.env.VUE_APP_API_KEY,
           },
           params: {
-            limit: 500,
+            limit: 200,
+            plan: 'TIER_FOUR',
           },
         };
         axios
@@ -214,7 +220,6 @@ export default {
                 name: team.name,
               });
             });
-            console.log(response.data.teams);
           })
           .catch((error) => {
             console.error('API request failed:', error.message);
